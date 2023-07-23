9e6af5d19a4d3c02f25c1e3d45d0d9a907aafd5f
Bric-R <chuxuange@gmail.com>
2023-07-23 23:31:53 +0800

    添加鉴权

--- scripts/mps/disable-mps.sh
+++ scripts/mps/disable-mps.sh
@@ -1,4 +1,20 @@
 #!/bin/bash
-echo -e "*****\033[34mDisable MPS\033[0m *****"
+
+# 检查当前用户是否为root用户
+if [[ $EUID -ne 0 ]]; then
+    echo "请使用sudo或以root用户身份执行此脚本。"
+    exit 1
+fi
+
+echo -e "***** \033[34mDisable MPS\033[0m *****"
 echo quit | nvidia-cuda-mps-control
 nvidia-smi -i 0 -c DEFAULT
+
+# 检查MPS是否已禁用
+mps_status=$(nvidia-smi -q -i 0 -d UTILIZATION | grep "MPS" | grep "Enabled")
+
+if [[ -z $mps_status ]]; then
+    echo "NVIDIA MPS已成功禁用。"
+else
+    echo "禁用NVIDIA MPS时出现问题，请检查相关配置。"
+fi
--- scripts/mps/enable-mps.sh
+++ scripts/mps/enable-mps.sh
@@ -1,5 +1,21 @@
 #!/bin/bash
-echo -e "*****\033[32mEnable MPS\033[0m *****"
+
+# 检查当前用户是否为root用户
+if [[ $EUID -ne 0 ]]; then
+    echo "请使用sudo或以root用户身份执行此脚本。"
+    exit 1
+fi
+
+echo -e "***** \033[32mEnable MPS\033[0m *****"
 export CUDA_VISIBLE_DEVICES="0"
 nvidia-smi -i 0 -c EXCLUSIVE_PROCESS
 nvidia-cuda-mps-control -d
+
+# 检查MPS是否已启用
+mps_status=$(nvidia-smi -q -i 0 -d UTILIZATION | grep "MPS" | grep "Enabled")
+
+if [[ -n $mps_status ]]; then
+    echo "NVIDIA MPS已成功启用。"
+else
+    echo "启用NVIDIA MPS时出现问题，请检查相关配置。"
+fi

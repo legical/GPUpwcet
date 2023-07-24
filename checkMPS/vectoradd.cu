9fb799df3843650bcec0915365c75df784d257ae
Bric-R <chuxuange@gmail.com>
2023-07-24 23:54:49 +0800

    改进测试代码

--- checkMPS/vectoradd.cu
+++ checkMPS/vectoradd.cu
@@ -4,18 +4,18 @@
 #include <ctime>
 #include <random>
 
-#define BLOCK_SIZE 32
-#define VECTOR_SIZE (1 << 24)
-#define OPERATIONS_PER_THREAD 512
+#define ARRAY_SIZE (1 << 18)
+#define BLOCK_NUM 2
+#define THREAD_PER_BLOCK 32
 
 __global__ void vectorAdd(double* a, double* b, double* c, int size) {
     int tid = blockIdx.x * blockDim.x + threadIdx.x;
 
-    for (int i = tid; i < size; i += blockDim.x * gridDim.x) {
-        for (int j = 0; j < OPERATIONS_PER_THREAD; j++) {  // 执行多个加法操作
+    for (int i = tid; i < size; i += BLOCK_NUM * THREAD_PER_BLOCK) {
+        for (int j = 0; j < THREAD_PER_BLOCK; j++) {  // 执行多个加法操作
             c[i] = a[i] + b[i];
         }
-        if (c[i] > VECTOR_SIZE) {
+        if (c[i] > ARRAY_SIZE) {
             printf("%f\n", c[i]);
         }
     }
@@ -26,35 +26,35 @@ int main() {
     double* devA, * devB, * devC;
 
     // 分配主机内存
-    hostA = new double[VECTOR_SIZE];
-    hostB = new double[VECTOR_SIZE];
-    hostC = new double[VECTOR_SIZE];
+    hostA = new double[ARRAY_SIZE];
+    hostB = new double[ARRAY_SIZE];
+    hostC = new double[ARRAY_SIZE];
 
     // 使用随机数生成器初始化输入向量
     std::random_device rd;
     std::mt19937 gen(rd());
     std::uniform_real_distribution<double> dis(0.0, 13.0);
-    for (int i = 0; i < VECTOR_SIZE; i++) {
+    for (int i = 0; i < ARRAY_SIZE; i++) {
         hostA[i] = dis(gen);
         hostB[i] = dis(gen);
     }
 
     // 分配设备内存
-    cudaMalloc((void**)&devA, VECTOR_SIZE * sizeof(double));
-    cudaMalloc((void**)&devB, VECTOR_SIZE * sizeof(double));
-    cudaMalloc((void**)&devC, VECTOR_SIZE * sizeof(double));
+    cudaMalloc((void**)&devA, ARRAY_SIZE * sizeof(double));
+    cudaMalloc((void**)&devB, ARRAY_SIZE * sizeof(double));
+    cudaMalloc((void**)&devC, ARRAY_SIZE * sizeof(double));
 
     // 将输入向量从主机内存复制到设备内存
-    cudaMemcpy(devA, hostA, VECTOR_SIZE * sizeof(double), cudaMemcpyHostToDevice);
-    cudaMemcpy(devB, hostB, VECTOR_SIZE * sizeof(double), cudaMemcpyHostToDevice);
+    cudaMemcpy(devA, hostA, ARRAY_SIZE * sizeof(double), cudaMemcpyHostToDevice);
+    cudaMemcpy(devB, hostB, ARRAY_SIZE * sizeof(double), cudaMemcpyHostToDevice);
 
     // 启动核函数
-    dim3 block(BLOCK_SIZE);
-    dim3 grid((VECTOR_SIZE + block.x - 1) / block.x / 2);  // 2个block
-    vectorAdd<<<grid, block>>>(devA, devB, devC, VECTOR_SIZE);
+    dim3 block(THREAD_PER_BLOCK);
+    dim3 grid(BLOCK_NUM);  // 2个block
+    vectorAdd<<<grid, block>>>(devA, devB, devC, ARRAY_SIZE);
 
-    // 将结果从设备内存复制到主机内存
-    cudaMemcpy(hostC, devC, VECTOR_SIZE * sizeof(double), cudaMemcpyDeviceToHost);
+    // 只拷贝一个结果，节省内存操作时间
+    cudaMemcpy(hostC, devC, sizeof(double), cudaMemcpyDeviceToHost);
 
     // 释放内存
     delete[] hostA;

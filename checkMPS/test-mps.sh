8c75d99f6b579551e25e6320bf6d811dda79628c
Bric-R <chuxuange@gmail.com>
2023-07-25 23:11:15 +0800

    添加解释

--- checkMPS/test-mps.sh
+++ checkMPS/test-mps.sh
@@ -41,7 +41,7 @@ echo "已启用 MPS 的测试执行时间：$execution_time_b 秒"
 # 比较执行时间
 echo ""
 time_difference=$(echo "$execution_time_a - $execution_time_b" | bc)
-if (($(echo "$time_difference > 1" | bc -l))); then
+if (($(echo "$time_difference > 2" | bc -l))); then
     echo -e "===== \033[32m测试结果：当前 GPU 支持 MPS\033[0m ====="
 else
     echo -e "===== \033[34m测试结果：当前 GPU 未启用或不支持 MPS\033[0m ====="

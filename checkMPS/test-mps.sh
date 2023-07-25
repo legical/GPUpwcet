968ec8af1d446240753b535bf4cef52200e21698
Bric-R <chuxuange@gmail.com>
2023-07-25 23:52:19 +0800

    改进提示

--- checkMPS/test-mps.sh
+++ checkMPS/test-mps.sh
@@ -5,15 +5,21 @@ source ~/.bashrc
 proj_dir=$(pwd)
 root_dir=$proj_dir/..
 
+source $root_dir/scripts/tool.sh
+
+check_GPU_Driver
+echo "--------------------- 测试 GPU$GPU_name 是否真的支持MPS ---------------------"
+echo ""
+
 # 编译 CUDA 程序
 nvcc vectoradd.cu -o vectoradd
 
-echo "1. 执行single测试..."
+echo "1. 基准测试：执行单个程序..."
 start_time=$(date +%s.%N)
 ./vectoradd
 end_time=$(date +%s.%N)
-execution_time_a=$(echo "$end_time - $start_time" | bc)
-echo "single测试时间：$execution_time_a 秒"
+basetime=$(echo "$end_time - $start_time" | bc)
+echo "基准测试时间：$basetime s"
 
 # 执行未启用 MPS 的测试
 echo "2. 执行未启用 MPS 的测试..."
@@ -21,7 +27,7 @@ start_time=$(date +%s.%N)
 ./vectoradd & ./vectoradd
 end_time=$(date +%s.%N)
 execution_time_a=$(echo "$end_time - $start_time" | bc)
-echo "未启用 MPS 的测试执行时间：$execution_time_a 秒"
+echo "未启用 MPS 的测试执行时间：$execution_time_a s"
 
 # 启用 MPS
 chmod +x $root_dir/scripts/mps/*.sh
@@ -36,7 +42,7 @@ start_time=$(date +%s.%N)
 ./vectoradd & ./vectoradd
 end_time=$(date +%s.%N)
 execution_time_b=$(echo "$end_time - $start_time" | bc)
-echo "已启用 MPS 的测试执行时间：$execution_time_b 秒"
+echo "已启用 MPS 的测试执行时间：$execution_time_b s"
 
 # 清理编译文件
 rm -rf vectoradd
@@ -45,9 +51,9 @@ rm -rf vectoradd
 echo ""
 time_difference=$(echo "$execution_time_a - $execution_time_b" | bc)
 if (($(echo "$time_difference > 2" | bc -l))); then
-    echo -e "===== \033[32m测试结果：当前 GPU 支持 MPS\033[0m ====="
+    echo -e "===== \033[32m测试结果：GPU$GPU_name 支持 MPS\033[0m ====="
 else
-    echo -e "===== \033[34m测试结果：当前 GPU 未启用或不支持 MPS\033[0m ====="
+    echo -e "===== \033[34m测试结果：GPU$GPU_name 未启用或不支持 MPS\033[0m ====="
 fi
 
 # 关闭 MPS
--- scripts/tool.sh
+++ scripts/tool.sh
@@ -1,1 +1,29 @@
 #!/bin/bash
+# 是否继续执行脚本
+function isGoon() {
+    read -p "Do you want to continue? [y/n] " input
+
+    case $input in
+    [yY]*) ;;
+
+    [nN]*)
+        exit
+        ;;
+    *)
+        echo -e "\033[31mJust enter y or n, please.\033[0m\n"
+        exit
+        ;;
+    esac
+}
+
+function check_GPU_Driver {
+    # 3060 or 1070
+    GPU_name_line=$(nvidia-smi -q | grep "Product Name")
+    if [ $? -ne 0 ]; then
+        # nvidia-smi 无输出，证明未安装NVIDIA Driver
+        echo -e "***** \033[34mPlease install NVIDIA Driver first !!!\033[0m *****"
+        exit 1
+    else
+        GPU_name=$(echo $GPU_name_line | awk -F ' ' '{print $NF}')
+    fi
+}
